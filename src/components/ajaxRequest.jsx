8ee622eee0e141655a2ccb72821b3fd4e23c2bf3
Dmitriy Otorov <otorov_dmitri@MacBook-Pro-otorov-dmitri.local>
2018-07-08 23:42:03 +0300

    Response check

--- src/components/ajaxRequest.jsx
+++ src/components/ajaxRequest.jsx
@@ -5,45 +5,52 @@ import '../scss/index.scss'
 
 export default class AjaxRequest extends Component {
 
-    constructor(props) {
-        super(props);
+constructor(props) {
+    super(props);
 
-        this.state = {
-            error: null,
-            status: false,
-            isLoaded: false,
-            items: []
-        };
+    this.state = {
+        error: null,
+        status: false,
+        isLoaded: false,
+        items: []
+    };
+}
+
+componentDidMount() {
+    fetch(`https://api.etherscan.io/api?module=account&action=balancemulti&address=${wallet[0].account}&tag=latest`)
+        .then(res => this.checkStatus(res))
+        .then(res => {return res.json()})
+        .then((result) => {
+            this.setState({
+            isLoaded: true,
+            status: result.status > 0 ? true : false,
+            items: result.result
+            });
+        },
+        (error) => {
+            this.setState({error});
+        }
+        )
+}
+
+checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        let error = new Error(response.statusText)
+        error.response = response
+        throw error
     }
+}
 
-    componentDidMount() {
-        fetch(`https://api.etherscan.io/api?module=account&action=balancemulti&address=${wallet[0].account}&tag=latest`)
-          .then(res => res.json())
-          .then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                status: result.status > 0 ? true : false,
-                items: result.result
-              });
-            },
-            (error) => {
-              this.setState({
-                error
-              });
-            }
-          )
-      }
+render() {
+    if (this.state.isLoaded && this.state.status) {
+        return (
+            <div>
+                <BalanceBlock items = {this.state.items} />
+            </div>
+        )
+    } else return null
+}
 
-    render() {
-
-        if (this.state.isLoaded && this.state.status) {
-            return (
-                <div>
-                    <BalanceBlock items = {this.state.items} />
-                </div>
-            )
-        } else return null
-
-    }
 }
